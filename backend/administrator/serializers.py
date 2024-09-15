c45aebb6a68caf1de0c5438b091623403567b7fa
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-09-15 08:17:36 +0100

    editted the blog serializer

--- backend/administrator/serializers.py
+++ backend/administrator/serializers.py
@@ -4,11 +4,18 @@ from blog.models import BlogPost
 
 User = get_user_model()
 
-
+        
 class AdminBlogPostSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    last_read = serializers.DateTimeField(read_only=True)
+    
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super(AdminBlogPostSerializer, self).create(validated_data)
 
 
 class UserSerializer(serializers.ModelSerializer):
--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -9,12 +9,15 @@ class CategorySerializer(serializers.ModelSerializer):
 class BlogPostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
     status = serializers.CharField(read_only=True)
-    last_read = serializers.IntegerField(read_only=True)
+    last_read = serializers.DateTimeField(read_only=True)
     
     class Meta:
         model = BlogPost
         fields = "__all__"
 
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super(BlogPostSerializer, self).create(validated_data)
 
 class UserImagesSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
