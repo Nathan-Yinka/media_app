4a91b9a47a31b12dda3d8846b883b78dbb1a662d
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-09-16 08:44:35 +0100

    edit the signup process

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,11 +5,42 @@ from rest_framework.validators import UniqueValidator
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=False, allow_blank=True, allow_null=True,
-                                   validators=[UniqueValidator(queryset=User.objects.all())])
-    mobile = serializers.CharField(required=False, allow_blank=True, allow_null=True,
-                                   validators=[UniqueValidator(queryset=User.objects.all())])
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        required=False, 
+        allow_blank=True, 
+        allow_null=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This email is already in use."
+            )
+        ],
+        error_messages={
+            'invalid': 'Please provide a valid email address.',
+            'blank': 'Email cannot be blank.'
+        }
+    )
+    
+    mobile = serializers.CharField(
+        required=False, 
+        allow_blank=True, 
+        allow_null=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This mobile number is already in use."
+            )
+        ],
+        error_messages={
+            'blank': 'Mobile number cannot be blank.'
+        }
+    )
+    
+    password = serializers.CharField(
+        write_only=True,
+    )
+
+
 
     class Meta:
         model = User
