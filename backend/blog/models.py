2f9ce73f82eb21a3473e8d309ff78d47b5aef725
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-09-16 10:40:57 +0100

    edit the blog serializr

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -26,6 +26,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='blog_posts')
     last_read = models.DateTimeField(auto_now=True)
+    view_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import BlogPost, Category, UserImages
+
+User = get_user_model()
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name','full_name']
+    def get_full_name(self, obj):
+        # Concatenate first_name and last_name to form full name
+        return f"{obj.first_name} {obj.last_name}".strip()  
+        
+
 class BlogPostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
     status = serializers.CharField(read_only=True)
     last_read = serializers.DateTimeField(read_only=True)
+    author_details = UserSerializer(read_only=True,source='author')
     
     class Meta:
         model = BlogPost
